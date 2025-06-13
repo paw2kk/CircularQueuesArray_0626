61fadb08528f7e7f400f13bf883569cc411f4a42
paw2kk <wndxakk@gmail.com>
2025-06-13 08:21:24 +0700

    menyelesaikan fungsi insert dan membuat fungsi remove

--- circular array.cpp
+++ circular array.cpp
@@ -22,7 +22,37 @@ class Queues {
             return;
         }
 
-       
+        //cek apakah antrian kosong
+        if (FRONT == -1){
+            FRONT = 0;
+            REAR = 0;
+        }
+        else{
+            //Jika REAR berada di posisi terakhir array,kembali ke awal array
+            if(REAR == max - 1)
+            REAR = 0;
+            else 
+            REAR = REAR +1;
+        }
+        queue_array[REAR] = num;
+    }
+    void remove(){
+        //Cek apakah antrian kosong
+        if(FRONT == -1){
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\The element deleted from the queue is: " << queue_aarray[FRONT];
+
+
+        //Cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR){
+            FRONT = -1;
+            REAR = -1;
+        }
+        else{
+            //Jika elemen yang dihapus berada di posisi terakhir array,kembali ke 
+        }
     }
 }
 
