3db97464f83a3a155c3a9d1893c779c4dbad5e91
paw2kk <wndxakk@gmail.com>
2025-06-13 09:52:00 +0700

    membenahi fungsi insert

--- circular_array.cpp
+++ circular_array.cpp
@@ -13,37 +13,31 @@ public:
         REAR = -1;
     }
 
-    void insert()
-    {
+   void insert(){
         int num;
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
 
-        // cek apakah antrian penuh
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
-        {
-            {
-                cout << "\nQueue overflow\n";
-                return;
-            }
-
-            // cek apakah antrian kosong
-            if (FRONT == -1)
-            {
-                FRONT = 0;
-                REAR = 0;
-            }
-            else
-            {
-                // Jika REAR berada di posisi terakhir array,kembali ke awal array
-                if (REAR == max - 1)
-                    REAR = 0;
-                else
-                    REAR = REAR + 1;
-            }
-            queue_array[REAR] = num;
+        //Cek apakah antrian penuh
+        if((FRONT == 0 && REAR == max -1)||(FRONT == REAR + 1)){
+            cout << "\nQueue overflow\n";
+            return;
         }
+        
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;     
+        }
+        else{
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max-1)
+            REAR = 0;
+            else
+            REAR = REAR +1;
+        }
+        queue_array[REAR] = num;
     }
     void remove()
     {
@@ -101,16 +95,16 @@ public:
             {
                 cout << queue_array[FRONT_position] << "   ";
                 FRONT_position++;
-            }
-
-        FRONT_position = 0;
+                 FRONT_position = 0;
 
         // iterasi dari awal array hinga REAR
         while (FRONT_position <= REAR_position)
-        {
+             {
             cout << queue_array[FRONT_position] << "   ";
             FRONT_position++;
-        }
+             }
+            }
+       
         cout << endl;
     }
 };
